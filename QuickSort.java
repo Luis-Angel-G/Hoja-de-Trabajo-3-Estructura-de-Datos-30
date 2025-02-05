240291d5efd1314c47160bf44b8eff47d9732908
MagicBag-tab <est24347@uvg.edu.gt>
2025-02-05 10:03:30 -0600

    Implementacion del metodo para el quicksort

--- QuickSort.java
+++ QuickSort.java
@@ -1,3 +1,49 @@
-public class QuickSort {
+/*Clase que lleva el quicksort
+ * Link de la información acerca del quicksort: https://www.geeksforgeeks.org/quick-sort-algorithm/
+ */
 
+import java.util.Arrays;
+
+public class QuickSort<T extends Comparable<T>> implements IGenericSort<T> {
+    @Override
+    public T[] sort(T[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+        return arr;
+    }
+
+    private void quickSort(T[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    private int partition(T[] arr, int low, int high) {
+        T pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j].compareTo(pivot) < 0) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private void swap(T[] arr, int i, int j) {
+        T temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = { 3, 1, 4, 1, 5, 9 };
+        QuickSort<Integer> sorter = new QuickSort<>();
+        Integer[] sortedArr = sorter.sort(arr);
+
+        System.out.println(Arrays.toString(sortedArr));
+    }
 }
