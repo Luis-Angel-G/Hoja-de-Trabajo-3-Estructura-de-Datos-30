2cf87f14b5ae536a0d4e1e0b0c5591933281e266
MagicBag-tab <est24347@uvg.edu.gt>
2025-02-06 06:06:52 -0600

    Agregue un test para poder arreglar los elementos int y otro para strings

--- MergeSortTest.java
+++ MergeSortTest.java
@@ -1,3 +1,32 @@
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class MergeSortTest {
+    private MergeSort<Integer> mergesort;
+    private MergeSort<String> mergesortstring;
+
+    @Before
+    public void setUp() {
+        mergesort = new MergeSort<>();
+        mergesortstring = new MergeSort<>();
+    }
+
+    @Test
+    public void ItShouldOrderTheValuesFromLowestToHighestInt() {
+        Integer[] arr = { 0, 2, 7, 4, 8, 1, 3 };
+        Integer[] result = { 0, 1, 2, 3, 4, 7, 8 };
+
+        assertArrayEquals(result, mergesort.sort(arr));
+    }
+
+    @Test
+    public void ItShouldOrderTheStringsInAlphabeticOrder() {
+        String[] arr = { "a", "t", "q", "s", "b" };
+        String[] result = { "a", "b", "q", "s", "t" };
+
+        assertArrayEquals(result, mergesortstring.sort(arr));
+    }
 
 }
