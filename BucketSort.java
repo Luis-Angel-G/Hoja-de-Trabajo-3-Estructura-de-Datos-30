c0ba09c0d3a756284e7a2282ef95d674ae23bbef
Luis <gir24753@uvg.edu.gt>
2025-02-13 11:38:17 -0600

    Refactor BucketSort to improve bucket initialization and distribution logic

--- BucketSort.java
+++ BucketSort.java
@@ -1,45 +1,55 @@
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 public class BucketSort<T extends Comparable<T>> implements IGenericSort<T> {
     @Override
     public T[] sort(T[] arr) {
         int n = arr.length;
-        if (n <= 0) {
-            return arr;
-        }
+        if (n <= 0) return arr;
 
         @SuppressWarnings("unchecked")
-        T[] output = (T[]) new Comparable[n];
-
-        @SuppressWarnings("unchecked")
-        LinkedList<T>[] buckets = new LinkedList[n];
+        List<T>[] buckets = new List[n];
 
         for (int i = 0; i < n; i++) {
             buckets[i] = new LinkedList<>();
         }
 
-        for (int i = 0; i < n; i++) {
-            int bucketIndex = (int) ((n - 1) * ((Number) arr[i]).doubleValue());
-            buckets[bucketIndex].add(arr[i]);
+        // Encontrar el mínimo y máximo
+        T min = arr[0], max = arr[0];
+        for (T num : arr) {
+            if (num.compareTo(min) < 0) min = num;
+            if (num.compareTo(max) > 0) max = num;
         }
 
-        for (int i = 0; i < n; i++) {
-            LinkedList<T> bucket = buckets[i];
+        // Normalización con conversión explícita (asumiendo Double.parseDouble())
+        double minValue = Double.parseDouble(min.toString());
+        double maxValue = Double.parseDouble(max.toString());
+        double range = maxValue - minValue;
+
+        // Distribuir en buckets
+        for (T num : arr) {
+            double value = Double.parseDouble(num.toString());
+            int bucketIndex = (int) ((value - minValue) / range * (n - 1));
+            buckets[bucketIndex].add(num);
+        }
+
+        // Ordenar cada bucket e insertarlos en el array final
+        int index = 0;
+        for (List<T> bucket : buckets) {
             insertionSort(bucket);
-            for (int j = 0; j < bucket.size(); j++) {
-                output[i++] = bucket.get(j);
+            for (T num : bucket) {
+                arr[index++] = num;
             }
         }
 
-        return output;
+        return arr;
     }
 
-    private void insertionSort(LinkedList<T> bucket) {
+    private void insertionSort(List<T> bucket) {
         for (int i = 1; i < bucket.size(); i++) {
             T key = bucket.get(i);
             int j = i - 1;
-
             while (j >= 0 && bucket.get(j).compareTo(key) > 0) {
                 bucket.set(j + 1, bucket.get(j));
                 j--;
