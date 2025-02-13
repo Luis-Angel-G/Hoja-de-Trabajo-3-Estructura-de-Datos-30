9799b90c8cb893be30a619ab3a4397ae9c55ad55
Luis <gir24753@uvg.edu.gt>
2025-02-13 11:42:40 -0600

    Refactor RadixSort for improved max element finding and counting logic; ensure InsertionSort ends with a newline

--- RadixSort.java
+++ RadixSort.java
@@ -3,34 +3,36 @@ import java.util.Arrays;
 public class RadixSort<T extends Comparable<T>> implements IGenericSort<T> {
     @Override
     public T[] sort(T[] arr) {
-        radixSort(arr, arr.length);
+        if (arr.length == 0) return arr;
+
+        int n = arr.length;
+        T max = findMax(arr);
+
+        for (int exp = 1; Integer.parseInt(max.toString()) / exp > 0; exp *= 10) {
+            countSort(arr, n, exp);
+        }
+
         return arr;
     }
 
-    private void radixSort(T[] arr, int n) {
-        if (arr.length == 0) {
-            return;
-        }
-
+    private T findMax(T[] arr) {
         T max = arr[0];
-        for (int i = 1; i < n; i++) {
-            if (arr[i].compareTo(max) > 0) {
-                max = arr[i];
+        for (T num : arr) {
+            if (num.compareTo(max) > 0) {
+                max = num;
             }
         }
-
-        for (int exp = 1; (int) max / exp > 0; exp *= 10) {
-            countSort(arr, n, exp);
-        }
+        return max;
     }
 
     private void countSort(T[] arr, int n, int exp) {
+        @SuppressWarnings("unchecked")
         T[] output = Arrays.copyOf(arr, n);
         int[] count = new int[10];
-        Arrays.fill(count, 0);
 
-        for (int i = 0; i < n; i++) {
-            count[((int) arr[i] / exp) % 10]++;
+        for (T num : arr) {
+            int index = (Integer.parseInt(num.toString()) / exp) % 10;
+            count[index]++;
         }
 
         for (int i = 1; i < 10; i++) {
@@ -38,13 +40,12 @@ public class RadixSort<T extends Comparable<T>> implements IGenericSort<T> {
         }
 
         for (int i = n - 1; i >= 0; i--) {
-            output[count[((int) arr[i] / exp) % 10] - 1] = arr[i];
-            count[((int) arr[i] / exp) % 10]--;
+            int index = (Integer.parseInt(arr[i].toString()) / exp) % 10;
+            output[count[index] - 1] = arr[i];
+            count[index]--;
         }
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = output[i];
-        }
+        System.arraycopy(output, 0, arr, 0, n);
     }
 
     public static void main(String[] args) {
